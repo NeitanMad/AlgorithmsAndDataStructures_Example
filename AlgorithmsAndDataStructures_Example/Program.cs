16b3dec75c2141fdc9d85bf0de61aec1b3dc4753
Mad <mad.nataniel@gmail.com>
2022-05-29 21:13:57 +0300

    Добавьте файлы проекта.

---
+++ AlgorithmsAndDataStructures_Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlgorithmsAndDataStructures_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
