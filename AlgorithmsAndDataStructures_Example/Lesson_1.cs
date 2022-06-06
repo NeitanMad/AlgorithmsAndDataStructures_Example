c2ad5f2c613f650a582b67780750044908a579b5
Mad <mad.nataniel@gmail.com>
2022-06-06 17:59:22 +0300

    Готово для сдачи.

---
+++ AlgorithmsAndDataStructures_Example/Lesson_1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsAndDataStructures
+{
+    class Lesson_1
+    {
+        public static string LessonName = "1. HomeWork_Lesson_1";
+
+
+        public static void OutputHomework()
+        {
+            string exit;
+            NumberCheck();
+            FunctionComplexity();
+            Fibonachi();
+            Console.Write("Конец домашней работы. Вернуться в начало? Y/N  ");
+            exit = Console.ReadLine().ToLower();
+            if (exit == "y")
+            {
+                Console.WriteLine("\n===========================================");
+                Program.Start();
+            }
+
+        }
+
+        public static void NumberCheck()
+        {
+            int d = 0;
+            int i = 2;
+
+            Console.WriteLine("Задание 1. Проверка на простое число.");
+            Console.Write("Введите целое положительное число: ");
+            int number = int.Parse(Console.ReadLine());
+            
+            while (i < number)
+            {
+                if (number % i == 0)
+                {
+                    d++;
+                }
+
+                i++;
+            }
+
+            if (d == 0)
+            {
+                Console.WriteLine($"Число {number} простое\n");
+            }
+            else
+            {
+                Console.WriteLine($"Число {number} непростое\n");
+            }
+        }
+
+        public static void FunctionComplexity()
+        {
+            Console.WriteLine("Задание 2. Расчитать сложность функции.");
+            Console.WriteLine("Сложность функции: O(N^3)\n");
+        }
+
+        public static void Fibonachi()
+        {
+            Console.WriteLine("Задание 3. Вычисления числа Фибоначчи.\n");
+            Console.Write("(Рекурсия) Введите номер элемента последовательности: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Элемент под номером {a} = {FibonachiRecursive(a)}\n");
+            Console.Write("(Цикл) Введите номер элемента последовательности: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Элемент под номером {b} = {FibonachiCycle(b)}");
+
+        }
+        public static int FibonachiRecursive(int n)
+        {
+            if (n == 0 || n == 1) return n;
+
+             return FibonachiRecursive(n - 1) + FibonachiRecursive(n - 2);
+
+        }
+
+        public static int FibonachiCycle(int n)
+        {
+            int result = 0;
+            int b = 1;
+            int tmp;
+
+            for (int i = 0; i < n; i++)
+            {
+                tmp = result;
+                result = b;
+                b += tmp;
+            }
+
+            return result;
+        }
+
+
+
+    }
+}
--- AlgorithmsAndDataStructures_Example/Program.cs
+++ AlgorithmsAndDataStructures_Example/Program.cs
@@ -1,12 +1,103 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
 
-namespace AlgorithmsAndDataStructures_Example
+namespace AlgorithmsAndDataStructures
 {
     class Program
     {
+
+        public static void Info()
+        {
+            StringBuilder NameOfTask = new StringBuilder();
+            NameOfTask.Append(Lesson_1.LessonName + "\n");
+            Console.WriteLine("Введите номер домашнего задания что бы откррыть его.");
+            Console.WriteLine("Доступные домашние работы: \n" + NameOfTask);
+        }
+
+        public static (int left, int top) GetCursorPosition()
+        {
+            return (Console.CursorLeft, Console.CursorTop);
+        }
+
+        public static void EnterComand()
+        {
+            (int left, int top) = GetCursorPosition();
+            StringBuilder Command = new StringBuilder();
+            char key;
+            Console.Write("№ работы: ");
+            do
+            {
+                key = Console.ReadKey().KeyChar;
+
+                if (key != 8 && key != 13) // не BackSpace && не Enter
+                {
+                    Command.Append(key);
+                }
+                (int currentLeft, int currentTop) = GetCursorPosition();
+
+                if (key == (char)8)
+                {
+                    if (Command.Length > 0)
+                    {
+                        Command.Remove(Command.Length - 1, 1);
+                    }
+
+                    if (currentLeft >= left)
+                    {
+                        Console.SetCursorPosition(currentLeft, top);
+                        Console.Write(" ");
+                        Console.SetCursorPosition(currentLeft, top);
+                    }
+                    else
+                    {
+                        Console.SetCursorPosition(left, top);
+                    }
+                }
+
+            } while (key != (char)13);
+
+            // Console.WriteLine(Command);
+            ParseCommandString(Command.ToString());
+
+        }
+
+        static void ParseCommandString(string Command)
+        {
+            string[] commandParams = Command.ToLower().Split(' ');
+            if (commandParams.Length > 0)
+            {
+
+                switch (commandParams[0])
+                {
+                    case "1":
+
+                        Lesson_1.OutputHomework();
+
+                        break;
+
+                    default:
+                        Console.WriteLine("Некоректый ввод.");
+                        EnterComand();
+
+                        break;
+                }
+
+            }
+        }
+
+        public static void Start()
+        {
+            Info();
+            EnterComand();
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Start();
         }
     }
 }
